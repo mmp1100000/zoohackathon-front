84ed99bf32525d5668a2d213448a2ac297188db9
Marco Muñoz Pérez <marcomunozperez@gmail.com>
2020-11-07 17:13:52 +0100

    Added test graph

--- src/views/Graphics.vue
+++ src/views/Graphics.vue
@@ -1,25 +1,61 @@
 <template>
   <div class="graphics">
-    GRAFICAS
+    <div class="hello" ref="chartdiv"></div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
 import * as am4maps from "@amcharts/amcharts4/maps";
 // eslint-disable-next-line @typescript-eslint/camelcase
 import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 // eslint-disable-next-line @typescript-eslint/camelcase
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+am4core.useTheme(am4themes_animated);
 
 export default Vue.extend({
   name: "Graphics",
   components: {},
-  beforeMount() {
-    am4core.useTheme(am4themes_animated);
+  mounted() {
+    const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+
+    chart.paddingRight = 20;
+
+    const data = [];
+    let visits = 10;
+    for (let i = 1; i < 366; i++) {
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+      data.push({
+        date: new Date(2018, 0, i),
+        name: "name" + i,
+        value: visits
+      });
+    }
+
+    chart.data = data;
+
+    const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    const series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+
+    series.tooltipText = "{valueY.value}";
+    chart.cursor = new am4charts.XYCursor();
+
+    const scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    chart.scrollbarX = scrollbarX;
+
+    this.chart = chart;
   }
 });
 </script>
-<style>
-</style>
+<style></style>
